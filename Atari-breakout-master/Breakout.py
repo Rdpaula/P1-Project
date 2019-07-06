d4a171ad912dbb31853853e17189922b3d49e43a
Raphael de Paula Fonseca <38382017+Rdpaula@users.noreply.github.com>
2019-07-05 23:26:21 -0300

    Add files via upload

---
+++ Atari-breakout-master/Breakout.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+import pygame
+from player import *
+from constantes import *
+import os
+DIRETORIO = os.getcwd()
+
+class Partida():
+    """
+        Classe que gerencia uma partida.
+    """
+    def __init__(self):
+        # Variável para checar se a partida está ativa ainda
+        self.estado = True 
+        self.CLOCK = pygame.time.Clock()
+        self.CLOCK.tick(60)
+
+    def update_graphics(self, janela, evento):
+        janela.fill(WHITE)
+        player.jogador.update(evento)
+    
+    def update(self, janela, evento):
+        """
+            Atualiza cada tick da partida
+        """
+        self.update_graphics(janela, evento)
+        pygame.display.update() # atualiza tela
+
+class Breakout():
+    """
+        Classe principal do jogo.
+    """
+    def __init__(self):
+        self.JANELA = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
+        pygame.display.set_caption("Atari Breakout")
+
+    def iniciar_jogo(self):
+        """
+            Inicia partida
+        """
+        player.jogador = player((DIRETORIO + "images/player.png"),330,500)        
+        self.run = True
+        self.partida_ativa = Partida()
+
+    def clean(self, janela):
+        janela.fill(WHITE)
+
+    def finalizar_jogo(self):
+        """
+            Finaliza Partida
+        """
+        self.run = False
+        self.partida_ativa = None
+
+    def checar_eventos(self):
+        """
+            Trata eventos de entrada e do estado da partida
+        """
+
+        if(not self.partida_ativa.estado):
+            self.finalizar_jogo()
+
+        for event in pygame.event.get():
+            # Se o usuário clicou no X da janela
+            if event.type == pygame.QUIT:
+                self.run = False
+                self.menu = False
+            self.partida_ativa.update(self.JANELA,event)
+            
+    def main(self):
+        """
+            Loop principal do jogo
+        """
+        
+        # Variáveis de loop para manter o jogo rodando
+        self.menu = True
+
+        while self.menu:
+            # TODO MENU
+
+            self.iniciar_jogo()
+            while self.run:
+                self.checar_eventos()
+                self.clean(self.JANELA)
+                player.jogador.update2()
+
+if __name__ == "__main__":
+    pygame.init() # inicia módulos do pygame
+    game = Breakout() # instancia a classe principal do jogo
+    game.main()
+    pygame.quit() # finaliza módulos pygame
---
+++ Atari-breakout-master/player.py
@@ -0,0 +1,54 @@
+import pygame
+import os 
+from constantes import *
+DIRETORIO = os.getcwd()
+
+class player(pygame.sprite.Sprite):
+    def __init__(self, sprite, pos_x, pos_y, velocidade=1):# initialing player
+        super(pygame.sprite.Sprite,self).__init__()
+        self.__initialposition_position = (pos_x,pos_y)
+        self.image = pygame.image.load(DIRETORIO+"/images/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+        self.velocidade = velocidade
+        self.vidas = 3
+        self.screen = pygame.display.set_mode((LARGURA_TELA,ALTURA_TELA))
+        self.left=False
+        self.right=False
+    def posicao_inicial(self): # definition of initial position
+        self.rect.x = self.__initial_position[0]
+        self.rect.y = self.__initial_position[1]
+
+    def die(self):
+        # self.som_losslife = pygame.mixer.Sound
+        self.posicao_inicial() # Reset position
+        self.vidas -= 1 # loss life
+
+    def update(self, evento):# moving the player
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                self.left = True
+            elif evento.key == pygame.K_RIGHT:
+                self.right = True
+        elif evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT:
+                self.left = False
+            elif evento.key == pygame.K_RIGHT:
+                self.right = False 
+        if self.right and self.rect.right < (LARGURA_TELA - self.velocidade):
+                self.rect.x += self.velocidade
+        elif self.left and self.rect.left > self.velocidade:
+                self.rect.x -= self.velocidade
+        self.screen.blit(self.image,(self.rect.x,self.rect.y)) 
+        pygame.display.update()
+    def update2(self):
+        if self.right and self.rect.right < (LARGURA_TELA - self.velocidade):
+                self.rect.x += self.velocidade
+        elif self.left and self.rect.left > self.velocidade:
+                self.rect.x -= self.velocidade
+        self.screen.blit(self.image,(self.rect.x,self.rect.y)) 
+        pygame.display.update()
+
+    
+    
