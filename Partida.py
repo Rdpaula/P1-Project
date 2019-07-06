af176529610ee9b70ea75f2fcc6e16e108c23eed
Lucas Albuquerque <52463587+brenner225@users.noreply.github.com>
2019-07-06 16:36:18 -0300

    Inicializador e gerador de mapas

--- Partida.py
+++ Partida.py
@@ -1,26 +1,59 @@
 # coding: utf-8
 
 import pygame
+import random
 from constantes import *
+from mapas import *
 from player import player
+from Blocos import Bloco
 from Bola import Bola
 
 class Partida():
     """
         Classe que gerencia uma partida.
     """
-    def __init__(self):
+    def __init__(self,mapa):
         # Variável para checar se a partida está ativa ainda
         self.estado = True 
         self.CLOCK = pygame.time.Clock()
         self.BLOCOS_GROUP = pygame.sprite.Group()
         self.JOGADOR_GROUP = pygame.sprite.GroupSingle()
         self.BOLA_GROUP = pygame.sprite.GroupSingle()
+        
+        # Inicializador do mapa
+        if mapa==0:
+            mapatemporario = self.gerador()
+            for bloco in mapatemporario:
+                self.BLOCOS_GROUP.add(bloco)
+        else:
+            for bloco in MAPAS[mapa]:
+                self.BLOCOS_GROUP.add(bloco)
+        
         jogador = player((DIRETORIO + "images/player.png"), 330, 500, 5)
         bola = Bola(LARGURA_TELA/2-TAMANHO_BOLA, ALTURA_TELA/2-TAMANHO_BOLA)
         self.JOGADOR_GROUP.add(jogador)
         self.BOLA_GROUP.add(bola)
+    
+    def gerador(self):
+        mapa_gerado = []
+        bloco=0
+        for i in range(1,4): # Quantidade de linhas
+            somalargura=2
+            while somalargura < LARGURA_TELA:
+                bloco+=1
+                aux = random.randint(39,80) # Intervalo de largura dos blocos
 
+                if aux%7 == 0:   # Critério de atribuição de poder
+                    mapa_gerado.append(Bloco(aux%5,somalargura,(30*i)+(3*(i-1)),aux,30,(aux%3)+1))
+                else:
+                    mapa_gerado.append(Bloco(aux%5,somalargura,(30*i)+(3*(i-1)),aux,30))
+                somalargura += aux + 4
+
+            somalargura = somalargura-aux+4
+            if LARGURA_TELA-somalargura-4 > 0:  # Se ainda ficou um pedaço da linha sem bloco
+                mapa_gerado.append(Bloco(aux%5,somalargura,(30*i)+3,LARGURA_TELA-somalargura-4,30))
+        
+        return mapa_gerado
 
     def update_graphics(self, janela):
         """
