27264da5909201c0b32b6d666bebdb7efaf89fab
Lucas Albuquerque <52463587+brenner225@users.noreply.github.com>
2019-07-06 16:39:10 -0300

    Classe bloco e arquivo para mapas

---
+++ Blocos.py
@@ -0,0 +1,17 @@
+import pygame
+import pygame.gfxdraw
+from constantes import *
+
+class Bloco(pygame.sprite.Sprite):
+    def __init__(self,vida,x,y,largura,altura,poder=0):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.Surface([largura,altura])
+        self.image.fill(CORES_VIDA[vida])
+
+        self.rect = self.image.get_rect()
+        
+        self.poder = poder
+
+        self.rect.x = x
+        self.rect.y = y
